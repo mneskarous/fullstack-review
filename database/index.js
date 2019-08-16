a05c7aa113fa4d8b7822ce50a64a9487a99525d7
Michael Eskarous <mneskarous@gmail.com>
2019-08-16 16:38:57 -0700

    implemented test to test database connection to server, function to save repo to db and schema to hold repo data

--- database/index.js
+++ database/index.js
@@ -1,16 +1,32 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('database connected');
+});
+
+let repoSchema = new mongoose.Schema({
   // TODO: your schema here!
+  owner: {
+    login: String
+  },
+  name: String,
+  url: String,
+  watchers: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+
+
+let save = (data) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  console.log('data',data);
+  return Repo.create(data)
 }
 
 module.exports.save = save;
