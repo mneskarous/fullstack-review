ffb9f5fa73f80093e4f57a5d19c5b6452e0470c9
Michael Eskarous <mneskarous@gmail.com>
2019-08-17 11:23:24 -0700

    Used request module to request repos for specific user from github API

--- helpers/github.js
+++ helpers/github.js
@@ -7,14 +7,27 @@ let getReposByUsername = (username) => {
 
   // The options object has been provided to help you out,
   // but you'll have to fill in the URL
-  
+
   let options = {
-    url: 'https://api.github.com',
+    url: 'https://api.github.com/repos/request/request',
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
+
+  callback(error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      var info = JSON.parse(body);
+      console.log(info.owner.login);
+      console.log(info.name);
+      console.log(info.html_url);
+      console.log(info.watchers);
+      response.send({info.owner.login, info.name,  info.html_url, info.watchers});
+    }
+  }
+
+  request(options, callback);
 
 }
 
